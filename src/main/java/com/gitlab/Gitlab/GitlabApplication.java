f4bf4ac1f1629f9657f0562feecdd509cf66ac8d
Pragash <knpragash@gmail.com>
2024-07-03 22:50:36 +0800

    Initial commit

---
+++ src/main/java/com/gitlab/Gitlab/GitlabApplication.java
@@ -0,0 +1,14 @@
+package com.gitlab.Gitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabApplication.class, args);
+		System.out.println("Charm........");
+	}
+
+}
---
+++ src/test/java/com/gitlab/Gitlab/GitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.Gitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
