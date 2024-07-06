2bb16bcc9250abfc320229c96ce5710bc53bb5d3
knpragash <knpragash@gmail.com>
2024-07-06 15:09:40 +0800

    upd 1

--- src/main/java/com/gitlab/Gitlab/GitlabApplication.java
+++ src/main/java/com/gitlab/Gitlab/GitlabApplication.java
@@ -8,7 +8,7 @@ public class GitlabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitlabApplication.class, args);
-		System.out.println("Charm........");
+		System.out.println("Charm........nn");
 	}
 
 }
