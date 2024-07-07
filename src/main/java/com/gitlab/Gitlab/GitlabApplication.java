c1ceb8a26e37977957c2b4e87bcafe499b2d3ca4
knpragash <knpragash@gmail.com>
2024-07-07 09:19:27 +0800

    well done..........Hha

--- src/main/java/com/gitlab/Gitlab/GitlabApplication.java
+++ src/main/java/com/gitlab/Gitlab/GitlabApplication.java
@@ -8,7 +8,7 @@ public class GitlabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitlabApplication.class, args);
-		System.out.println("Charm........nnbb");
+		System.out.println("Charm........welldone");
 	}
 
 }
